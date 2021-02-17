f600f2dc12d4e30706595a4c422256df83997c71
forsrc <forsrc@gmail.com>
2021-02-17 20:50:03 +0900

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,7 +1,7 @@
 #  /boot/cmdline.txt   +++ cgroup_enable=cpuset cgroup_enable=memory
 
 
-sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=0.0.0.0 --apiserver-cert-extra-sans=192.168.1.10,192.168.2.10,10.244.0.0 --ignore-preflight-errors=all
+sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=0.0.0.0 --apiserver-cert-extra-sans=192.168.1.10,192.168.2.10 --ignore-preflight-errors=all
 
 
 mkdir -p $HOME/.kube
