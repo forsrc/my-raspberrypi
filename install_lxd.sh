ed2383f555957ce4ad9331aec581222e0650415a
forsrc <forsrc@gmail.com>
2021-02-18 14:24:04 +0900

    Update install_lxd.sh

--- install_lxd.sh
+++ install_lxd.sh
@@ -5,4 +5,25 @@ sudo snap install core
 sudo snap install lxd
 
 
- sudo lxd init --network-address 0.0.0.0 --network-port 8443 --auto
+sudo lxd init --network-address 0.0.0.0 --network-port 8443 --auto
+
+##################
+
+
+sudo systemctl status snap-lxd-19390.mount
+sudo systemctl status snap.lxd.activate.service
+sudo systemctl status snap.lxd.daemon.unix.socket
+sudo systemctl status snap.lxd.daemon.service
+
+
+
+
+sudo systemctl stop snap.lxd.daemon.unix.socket
+sudo systemctl stop snap.lxd.daemon.service
+sudo systemctl stop snap-lxd-19390.mount
+sudo systemctl stop snap.lxd.activate.service
+
+sudo systemctl disable snap-lxd-19390.mount
+sudo systemctl disable snap.lxd.activate.service
+sudo systemctl disable snap.lxd.daemon.unix.socket
+sudo systemctl disable snap.lxd.daemon.service
