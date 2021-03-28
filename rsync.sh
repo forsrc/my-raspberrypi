2b0a34267e8fc670b60ff822bbfd25c31e399ddd
forsrc <forsrc@gmail.com>
2021-03-28 13:30:21 +0900

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,6 @@
-sudo rsync -aqxP /var/lib/docker /media/nfs/pi/pi-01/var/lib/docker
-sudo rsync -aqxP /var/lib/docker /media/nfs/pi/pi-02/var/lib/docker
+#sudo rsync -aqxP /var/lib/docker /media/nfs/pi/pi-01/var/lib/docker
+#sudo rsync -aqxP /var/lib/docker /media/nfs/pi/pi-02/var/lib/docker
+
+sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-00/var/lib/docker
+sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-01/var/lib/docker
+sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-02/var/lib/docker
