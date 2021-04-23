1a4fc3d0e5184896193b3c450f5e170130d12d0a
forsrc <forsrc@gmail.com>
2021-04-24 05:10:59 +0900

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -1,6 +1,7 @@
 sudo apt-get install -y libhdf5-dev libc-ares-dev libeigen3-dev gcc gfortran python-dev libgfortran5 \
                           libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev \
-                          liblapack-dev cython libatlas-base-dev openmpi-bin libopenmpi-dev python3-dev
+                          liblapack-dev cython libatlas-base-dev openmpi-bin libopenmpi-dev python3-dev python-numpy
+
 sudo pip3 install keras_applications==1.0.8 --no-deps
 sudo pip3 install keras_preprocessing==1.1.0 --no-deps
 sudo pip3 install h5py==2.10.0
