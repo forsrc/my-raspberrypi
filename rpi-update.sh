be1e6cf784240bd4b14bc78a0304ff2fc2c20802
forsrc <forsrc@gmail.com>
2021-05-01 04:09:12 +0900

    Update rpi-update.sh

--- rpi-update.sh
+++ rpi-update.sh
@@ -1,3 +1,7 @@
 sudo rpi-update
 sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/local/bin/rpi-source && sudo chmod +x /usr/local/bin/rpi-source && /usr/local/bin/rpi-source -q --tag-update
 sudo rpi-source
+
+
+
+sudo apt-get install raspberrypi-kernel-headers
