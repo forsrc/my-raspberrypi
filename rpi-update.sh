6f3a9d2542683a74a868172a223147c875e21cd1
forsrc <forsrc@gmail.com>
2021-05-01 02:24:04 +0900

    Create rpi-update.sh

---
+++ rpi-update.sh
@@ -0,0 +1,3 @@
+sudo rpi-update
+sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/local/bin/rpi-source && sudo chmod +x /usr/local/bin/rpi-source && /usr/local/bin/rpi-source -q --tag-update
+sudo rpi-source
