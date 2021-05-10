1a5fb8ff46e94228b00f8a875c9f0f12e6021c44
forsrc <forsrc@gmail.com>
2021-05-10 14:25:45 +0900

    Update raspivid.sh

--- raspivid.sh
+++ raspivid.sh
@@ -1,1 +1,5 @@
 raspivid -t 0 -b 25000000 -fps 30 -w 800 -h 600 -o - | nc -lkv4 8080
+
+
+raspivid -o - -t 0 -b 25000000 -n -w 800 -h 600 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+
