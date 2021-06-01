af0c781ec3f16501233132a3327c4f9bf945d48c
forsrc <forsrc@gmail.com>
2021-06-01 16:11:35 +0900

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -9,19 +9,22 @@ sudo systemctl start systemd-resolved
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
-sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=0.0.0.0 --apiserver-cert-extra-sans=192.168.1.10 --ignore-preflight-errors=all
-
+sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=0.0.0.0      --apiserver-cert-extra-sans=192.168.1.10 --ignore-preflight-errors=all
+sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=192.168.1.10 --apiserver-cert-extra-sans=pi-00,pi-01,p1-02,192.168.1.10,192.168.2.10,0.0.0.0 --ignore
+-preflight-errors=all
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-sudo kubeadm join 192.168.1.10:6443 --token edmtvn.8at086ar9c7f94uw \
-        --discovery-token-ca-cert-hash sha256:d78e8cac64226ca303bd8021dea21505e1402d14262868a69f65c65c77e7bd34  --ignore-preflight-errors=all
+sudo kubeadm join 192.168.1.10:6443 --token rfj1s9.ju792gulg6w8vcii \
+        --discovery-token-ca-cert-hash sha256:3c824e95411ad9118461d1646f7a313943bef1afa56818c19394998e8cf8e0c6 --ignore-preflight-errors=all
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
+
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -81,3 +84,29 @@ rm -rf /var/lib/cni/flannel/* && rm -rf /var/lib/cni/networks/cbr0/* && ip link 
 
 apt remove -y kubelet kubeadm kubectl
 apt-get install -y kubelet kubeadm kubectl
+
+
+cat <<EOF | kubectl delete -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+cat <<EOF | kubectl delete -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+
