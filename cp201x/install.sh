5c71dd896022546457c91deefd3d3a27325246f8
forsrc <forsrc@gmail.com>
2021-06-08 01:33:39 +0900

    Update install.sh

--- cp201x/install.sh
+++ cp201x/install.sh
@@ -1,7 +1,8 @@
 sudo apt-get install raspberrypi-kernel-headers -y
 
 make ( your cp210x driver )
-cp cp210x.ko to /lib/modules//kernel/drivers/usb/serial
+#cp cp210x.ko to /lib/modules/5.10.17+/kernel/drivers/usb/serial
+sudo cp cp210x.ko /lib/modules/5.10.17+/kernel/drivers/usb/serial
 #insmod /lib/modules/<kernel-version/kernel/drivers/usb/serial/usbserial.ko
-insmod /lib/modules/5.10.17+/kernel/drivers/usb/serial/usbserial.ko
-insmod cp210x.ko
+sudo insmod /lib/modules/5.10.17+/kernel/drivers/usb/serial/usbserial.ko
+sudo insmod cp210x.ko
