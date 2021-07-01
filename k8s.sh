b4efe3514181ae0648c1f63e398a20d578f4a868
forsrc <forsrc@gmail.com>
2021-07-01 20:07:03 +0900

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -21,7 +21,7 @@ sudo systemctl start systemd-resolved
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
-sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=192.168.1.10 --apiserver-cert-extra-sans=192.168.1.10 --ignore-preflight-errors=all
+sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=192.168.1.10 --apiserver-cert-extra-sans=192.168.1.10
 #sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=0.0.0.0      --apiserver-cert-extra-sans=192.168.1.10 --ignore-preflight-errors=all
 #sudo kubeadm init  --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=192.168.1.10 --service-cidr 10.96.0.0/12 --apiserver-cert-extra-sans=pi-00,pi-01,p1-02,192.168.1.10,192.168.2.10,10.96.0.1,10.96.0.0 --ignore-preflight-errors=all
 
@@ -29,8 +29,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-sudo kubeadm join 192.168.1.10:6443 --token 0m1i25.ojiuzcua8zogsy4v \
-        --discovery-token-ca-cert-hash sha256:96ac5be3db98162d08a804e695e6237aa08b446200dd4178e99e1e720f5e1dcc
+sudo kubeadm join 192.168.1.10:6443 --token u4vday.rdtx9viwxvuw9w32 \
+        --discovery-token-ca-cert-hash sha256:bd332860d8b1463f0930cabdaf3131e6bf5ee10633bf3e413d4ef9650bd4ac56
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
@@ -131,6 +131,8 @@ sudo systemctl stop kubelet
 sudo systemctl stop docker
 
 sudo systemctl stop docker.socket
+
+sudo rm -rf  /etc/kubernetes/*
 
 sudo rm -rf /var/lib/cni/
 
