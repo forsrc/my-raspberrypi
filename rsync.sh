909edaf217834fed387281edd297efa0445e12bc
forsrc <forsrc@gmail.com>
2021-07-14 19:47:20 +0900

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -4,3 +4,8 @@
 sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-00/var/lib/docker
 sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-01/var/lib/docker
 sudo rsync -axP /var/lib/docker /media/nfs/pi/pi-02/var/lib/docker
+
+
+
+sudo rsync -avxHAXS --exclude=/media --numeric-ids --info=progress2 --progress / /media/nfs/pi/pi-01
+sudo rsync -avxHAXS --exclude=/media --numeric-ids --info=progress2 --progress / /media/nfs/pi/pi-02
